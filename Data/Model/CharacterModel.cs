7461a55192abd97a81668c7ed50e291c5fb6df9d
xtrod <christiancaleroerazo@gmail.com>
2022-11-17 17:08:43 +0000

    Updated model

--- Data/Model/CharacterModel.cs
+++ Data/Model/CharacterModel.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterModel
 {
     public string name;
-    public float speed;
+    public string idToken;
+    public int currentLevel;
+    public int xp;
+    public int gold;
+    public int gems;
+    public int strong;
+    public int speed;
+    public int defense;
     public float currentPositionX;
     public float currentPositionY;
+
+    public List<InventoryItem> inventoryItems;
+    public DateTime latestLoggin;
 }
