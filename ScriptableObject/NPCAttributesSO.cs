baa327239864beeebb6d0a3bb626308ea9884f3e
xtrod <christiancaleroerazo@gmail.com>
2023-01-28 02:43:51 +0000

    Updated NPCAttributesSO. added new values.

--- ScriptableObject/NPCAttributesSO.cs
+++ ScriptableObject/NPCAttributesSO.cs
@@ -5,35 +5,28 @@ using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Greenvillex/NPCAttributes")]
 public class NPCAttributesSO : ScriptableObject {
-    public string nameUser;
-    public float speedUser;
-    public float jumpPower;
-    public float lifePoints;
-    public float manaPoints;
+    [SerializeField] private string name;
+    [SerializeField] private string idToken;
+    [SerializeField] private int currentLevel;
+    [SerializeField] private int xp;
+    [SerializeField] private int gold;
+    [SerializeField] private int gems;
+    [SerializeField] private int strong;
+    [SerializeField] private int speed;
+    [SerializeField] private int defense;
+    [SerializeField] private int maxLifesPoints;
+    [SerializeField] private int maxManaPoints;
+
+    public string Name { get => name; set => name = value; }
+    public string IdToken { get => idToken; set => idToken = value; }
+    public int CurrentLevel { get => currentLevel; set => currentLevel = value; }
+    public int Xp { get => xp; set => xp = value; }
+    public int Gold { get => gold; set => gold = value; }
+    public int Gems { get => gems; set => gems = value; }
+    public int Strong { get => strong; set => strong = value; }
+    public int Speed { get => speed; set => speed = value; }
+    public int Defense { get => defense; set => defense = value; }
+    public int MaxLifesPoints { get => maxLifesPoints; set => maxLifesPoints = value; }
+    public int MaxManaPoints { get => maxManaPoints; set => maxManaPoints = value; }
     //public List<ItemSO> listItems;
-
-    public string GetNameUser()
-    {
-        return nameUser;
-    }
-
-    public float GetSpeedUser()
-    {
-        return speedUser;
-    }
-
-    public float GetJumpPower()
-    {
-        return jumpPower;
-    }
-
-    public float GetLifePoints()
-    {
-        return lifePoints;
-    }
-
-    public float GetManaPoints()
-    {
-        return manaPoints;
-    }
 }
